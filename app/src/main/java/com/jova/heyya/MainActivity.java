7f26c88276ef2d76b24f13e08419477df6ce7393
Jovial248 <jovialjohngeorge248@gmail.com>
2018-01-31 13:51:50 +0530

    First Commit

---
+++ app/src/main/java/com/jova/heyya/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jova.heyya;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ASUS on 31-01-2018.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
